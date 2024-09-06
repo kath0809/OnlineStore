818a03714609044680083f79614662d0b8f4ff18
kath0809 <kath014@student.kristiania.no>
2024-09-06 15:34:30 +0200

    Initial Commit

---
+++ OnlineStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineStore
+//
+//  Created by Karima Thingvold on 06/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineStore/OnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreApp.swift
+//  OnlineStore
+//
+//  Created by Karima Thingvold on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
