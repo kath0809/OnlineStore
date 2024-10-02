ae0b4ea16ec29a2c5c1d5d350c0cf308a2ed817e
kath0809 <kath014@student.kristiania.no>
2024-10-02 13:57:44 +0200

    Added modifier for "3D art"

--- OnlineStore/View+Viewmodifiers.swift
+++ OnlineStore/View+Viewmodifiers.swift
@@ -32,13 +32,102 @@ extension View {
     }
 }
 
+
+
+struct WordArt3DModifier: ViewModifier {
+    
+    let baseColor: Color
+    let shadowOffset: CGFloat
+    
+    func body(content: Content) -> some View {
+        
+        ZStack {
+            content
+                .offset(x: shadowOffset * 2, y: shadowOffset * 2)
+                .foregroundStyle(baseColor)
+            
+            content
+                .offset(x: shadowOffset, y: shadowOffset)
+                .foregroundStyle(baseColor)
+            
+            content
+                .foregroundStyle(.white)
+            
+            
+        }
+    }
+}
+
+// Kan legge det inn i den andre extension.
+extension View {
+    
+    func wordArt3D(baseColor: Color = .green,
+                   shadowOffset: CGFloat = 1.5)
+    -> some View {
+        return self.modifier(WordArt3DModifier(baseColor: baseColor, shadowOffset: shadowOffset))
+    }
+}
+
+extension View {
+    
+    func measure() -> some View {
+        overlay {
+            GeometryReader { geometry in
+                
+                
+                Rectangle()
+                    .stroke(.pink, style:
+                                StrokeStyle(lineWidth: 2))
+                    . overlay {
+                        Text(
+                    """
+                    \(Double(geometry.size.width).formatted(.number.precision(
+                        .fractionLength(2))))
+                ,
+                    \(Double(geometry.size.height)
+                        .formatted(.number
+                        .precision(
+                        .fractionLength(2))))
+)
+""")
+                        
+                        .font(.headline)
+                        .kerning(-0.5)
+                        .padding(.vertical, 1)
+                        .padding(.horizontal, 2)
+                        .foregroundStyle(.white)
+                    }
+            }
+        }
+    }
+}
+
+
+
 #Preview {
-    
-    // Bruker ikke fargene som er satt default, men sender inn egne.
-    Text("Hello, World!")
-        .rainbowStyle(colors:[.blue, .black, .yellow, .red])
-    
-    // Bruker den standarden satt i rainbowStyle
-    Text("Hello, World!")
-        .rainbowStyle()
+    ZStack {
+        Color.black
+            .opacity(0.9)
+            .ignoresSafeArea()
+        VStack {
+            
+            Text("Word art 3D!")
+                //.font(.largeTitle)
+                .font(.system(size: 60))
+                .wordArt3D()
+                //.wordArt3D(shadowOffset: 2)
+                .measure()
+            
+            Divider()
+            
+                // Bruker ikke fargene som er satt default, men sender inn egne.
+            Text("Hello, Custom!")
+                .rainbowStyle(colors:[.blue, .black, .yellow, .red])
+            
+                // Bruker den standarden satt i rainbowStyle
+            Text("Hello, Worl")
+                .rainbowStyle()
+                
+        }
+    }
 }
